fc34457e9f52cd415727adba33a41bbbc8e04008
Lucinde <lucinde@lucinde.nl>
2025-12-12 10:40:15 +0100

    Add DayLog Model, Migration, Factory and Seeder

--- database/seeders/TestUserSeeder.php
+++ database/seeders/TestUserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\DayLog;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -13,18 +14,53 @@ class TestUserSeeder extends Seeder
     {
         $roles = Role::all();
 
+        $fysio = null;
+
+        // create fysio
         foreach ($roles as $role) {
+            if ($role->name === 'fysio') {
+                $email = $role->name . '@example.com';
+                $fysio = User::firstOrCreate(
+                    ['email' => $email],
+                    [
+                        'name' => ucfirst($role->name) . ' Test',
+                        'password' => Hash::make('password123'),
+                        'email_verified_at' => now(),
+                    ]
+                );
+                $fysio->assignRole($role);
+                break;
+            }
+        }
+
+        // create other roles
+        foreach ($roles as $role) {
+            if ($role->name === 'fysio') {
+                continue; // fysio is already there
+            }
+
             $email = $role->name . '@example.com';
 
-            if (!User::where('email', $email)->exists()) {
-                $user = User::create([
-                    'name'     => ucfirst($role->name) . ' Test',
-                    'email'    => $email,
+            $user = User::firstOrCreate(
+                ['email' => $email],
+                [
+                    'name' => ucfirst($role->name) . ' Test',
                     'password' => Hash::make('password123'),
                     'email_verified_at' => now(),
-                ]);
+                ]
+            );
 
-                $user->assignRole($role);
+            $user->assignRole($role);
+
+            // add relation for client and fysio
+            if ($user->hasRole('client') && $fysio) {
+                $user->therapist_id = $fysio->id;
+                $user->save();
+
+                DayLog::factory()
+                    ->forUser($user)
+                    ->count(7)
+                    ->create();
             }
         }
     }
