ef44714d9ae56de02190e8a490f6827e8aa6942b
Lucinde <lucinde@lucinde.nl>
2025-12-30 17:15:12 +0100

    Make painlog create action

--- app/Filament/Resources/DayLogs/RelationManagers/PainlogsRelationManager.php
+++ app/Filament/Resources/DayLogs/RelationManagers/PainlogsRelationManager.php
@@ -2,7 +2,14 @@
 
 namespace App\Filament\Resources\DayLogs\RelationManagers;
 
+use App\Enums\PainLocation;
+use Filament\Actions\CreateAction;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\TimePicker;
 use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Forms;
+use Filament\Schemas\Schema;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Model;
@@ -16,6 +23,33 @@ class PainlogsRelationManager extends RelationManager
     public static function getTitle(Model $ownerRecord, string $pageClass): string
     {
         return __('daylog.pain_logs.title');
+    }
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema->components([
+                TimePicker::make('start_time')
+                    ->label(__('daylog.pain_logs.start_time'))
+                    ->seconds(false)
+                    ->required(),
+                TimePicker::make('end_time')
+                    ->label(__('daylog.pain_logs.end_time'))
+                    ->seconds(false)
+                    ->required(),
+                Select::make('location')
+                    ->label(__('daylog.pain_logs.location'))
+                    ->options(PainLocation::class)
+                    ->multiple()
+                    ->required(),
+                TextInput::make('intensity')
+                    ->label(__('daylog.pain_logs.intensity'))
+                    ->numeric()
+                    ->minValue(1)
+                    ->maxValue(10),
+                TextInput::make('notes')
+                    ->label(__('daylog.pain_logs.notes'))
+                    ->maxLength(500),
+            ]);
     }
 
     public function table(Table $table): Table
@@ -41,6 +75,10 @@ class PainlogsRelationManager extends RelationManager
                 TextColumn::make('notes')
                     ->label(__('daylog.pain_logs.notes'))
                     ->limit(50),
+            ])
+            ->headerActions([
+                CreateAction::make()
+                    ->label(__('daylog.pain_logs.create')),
             ])
             ->emptyStateHeading(__('daylog.pain_logs.empty_heading'))
             ->emptyStateDescription(__('daylog.pain_logs.empty_description'));
