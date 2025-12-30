a22c102e2f64ee02e1857057924f4bfbedb27816
Lucinde <lucinde@lucinde.nl>
2025-12-30 15:41:03 +0100

    Make Model and Migration for PainLog

---
+++ app/Models/PainLog.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\PainLocation;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\AsEnumCollection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PainLog extends Model
+{
+    use softDeletes;
+
+    protected $fillable = [
+        'day_log_id',
+        'start_time',
+        'end_time',
+        'duration_minutes',
+        'location',
+        'intensity',
+        'notes',
+    ];
+
+    public function dayLog(): BelongsTo
+    {
+        return $this->belongsTo(DayLog::class);
+    }
+
+    protected static function booted(): void
+    {
+        static::saving(function ($painLog) {
+            if ($painLog->start_time && $painLog->end_time) {
+                $painLog->duration_minutes =
+                    Carbon::parse($painLog->start_time)
+                        ->diffInMinutes(
+                            Carbon::parse($painLog->end_time)
+                        );
+            }
+        });
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'location' => AsEnumCollection::of(PainLocation::class),
+        ];
+    }
+}
