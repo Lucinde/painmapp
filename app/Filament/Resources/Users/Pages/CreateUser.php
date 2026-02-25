c623490fd22670d978b9cbbd6c1be7b1022e2f61
Lucinde <lucinde@lucinde.nl>
2026-02-25 19:48:37 +0100

    Fallback to client role when no role is chosen

--- app/Filament/Resources/Users/Pages/CreateUser.php
+++ app/Filament/Resources/Users/Pages/CreateUser.php
@@ -3,9 +3,14 @@
 namespace App\Filament\Resources\Users\Pages;
 
 use App\Filament\Resources\Users\UserResource;
+use App\Models\User;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Support\Facades\Log;
+use Spatie\Permission\Models\Role;
 
+/**
+ * @property User $record
+ */
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
@@ -13,5 +18,16 @@ class CreateUser extends CreateRecord
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
+    }
+
+    protected function afterCreate(): void
+    {
+        $clientRole = Role::where('name', 'client')
+            ->where('guard_name', 'web')
+            ->first();
+
+        if ($clientRole && $this->record->roles->isEmpty()) {
+            $this->record->assignRole($clientRole);
+        }
     }
 }
