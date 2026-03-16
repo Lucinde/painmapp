bcceb93bf2c92b83495af666b311ba3fd78c7298
Lucinde <lucinde@lucinde.nl>
2026-03-16 12:02:02 +0100

    Update permissions seeder

--- database/seeders/ShieldSeeder.php
+++ database/seeders/ShieldSeeder.php
@@ -5,21 +5,15 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use BezhanSalleh\FilamentShield\Support\Utils;
 use Spatie\Permission\PermissionRegistrar;
-use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\Permission;
 
 class ShieldSeeder extends Seeder
 {
     public function run(): void
     {
-        // Cache van Spatie permissies resetten
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
-        // Rollen met bijbehorende permissies
-        $rolesWithPermissions = '[{"name":"super_admin","guard_name":"web","permissions":["ViewAny:Role","View:Role","Create:Role","Update:Role","Delete:Role","Restore:Role","ForceDelete:Role","ForceDeleteAny:Role","RestoreAny:Role","Replicate:Role","Reorder:Role","ViewAny:User","View:User","Create:User","Update:User","Delete:User","Restore:User","ForceDelete:User","ForceDeleteAny:User","RestoreAny:User","Replicate:User","Reorder:User","ViewAny:DayLog","View:DayLog","Create:DayLog","Update:DayLog","ForceDeleteAny:DayLog","ForceDelete:DayLog","Delete:DayLog","Restore:DayLog","RestoreAny:DayLog","Replicate:DayLog","Reorder:DayLog","ViewOwn:DayLog","ViewClient:DayLog","ViewClient:User","View:ActiveClients","View:PainActivityStats","View:PainActivityChart"]},{"name":"fysio","guard_name":"web","permissions":["View:Role","View:User","Create:User","Update:User","Delete:User","View:DayLog","Create:DayLog","Update:DayLog","ViewOwn:DayLog","ViewClient:DayLog","ViewClient:User","View:ActiveClients"]},{"name":"client","guard_name":"web","permissions":["View:User","Update:User","View:DayLog","Create:DayLog","Update:DayLog","ForceDelete:DayLog","Delete:DayLog","Restore:DayLog","Replicate:DayLog","Reorder:DayLog","ViewOwn:DayLog","View:PainActivityStats","View:PainActivityChart"]}]';
-
-        // Directe permissies
-        $directPermissions = '{"36":{"name":"ViewAssigned:User","guard_name":"web"},"37":{"name":"ViewOwn:User","guard_name":"web"}}';
+        $rolesWithPermissions = '[{"name":"super_admin","guard_name":"web","permissions":["ViewAny:Role","View:Role","Create:Role","Update:Role","Delete:Role","Restore:Role","ForceDelete:Role","ForceDeleteAny:Role","RestoreAny:Role","Replicate:Role","Reorder:Role","ViewAny:User","View:User","Create:User","Update:User","Delete:User","Restore:User","ForceDelete:User","ForceDeleteAny:User","RestoreAny:User","Replicate:User","Reorder:User","ViewAny:DayLog","View:DayLog","Create:DayLog","Update:DayLog","ForceDeleteAny:DayLog","ForceDelete:DayLog","Delete:DayLog","Restore:DayLog","RestoreAny:DayLog","Replicate:DayLog","Reorder:DayLog","ViewOwn:DayLog","ViewClient:DayLog","View:Dashboard"]},{"name":"fysio","guard_name":"web","permissions":["View:Role","View:User","Create:User","Update:User","Delete:User","View:DayLog","Create:DayLog","Update:DayLog","ViewOwn:DayLog","ViewClient:DayLog","ViewClient:User","View:ActiveClients"]},{"name":"client","guard_name":"web","permissions":["View:User","Update:User","View:DayLog","Create:DayLog","Update:DayLog","ForceDelete:DayLog","Delete:DayLog","Restore:DayLog","Replicate:DayLog","Reorder:DayLog","ViewOwn:DayLog","View:PainActivityStats","View:PainActivityChart"]}]';
+        $directPermissions = '[]';
 
         static::makeRolesWithPermissions($rolesWithPermissions);
         static::makeDirectPermissions($directPermissions);
@@ -27,32 +21,25 @@ class ShieldSeeder extends Seeder
         $this->command->info('Shield Seeding Completed.');
     }
 
-    /**
-     * Rollen en hun permissies aanmaken/syncen.
-     *
-     * @param string $rolesWithPermissions JSON string van rollen + permissies
-     */
     protected static function makeRolesWithPermissions(string $rolesWithPermissions): void
     {
-        $rolePlusPermissions = json_decode($rolesWithPermissions, true);
-
-        if (!empty($rolePlusPermissions)) {
-            /** @var class-string<Role> $roleModel */
+        if (! blank($rolePlusPermissions = json_decode($rolesWithPermissions, true))) {
+            /** @var Model $roleModel */
             $roleModel = Utils::getRoleModel();
-            /** @var class-string<Permission> $permissionModel */
+            /** @var Model $permissionModel */
             $permissionModel = Utils::getPermissionModel();
 
-            foreach ($rolePlusPermissions as $roleData) {
+            foreach ($rolePlusPermissions as $rolePlusPermission) {
                 $role = $roleModel::firstOrCreate([
-                    'name' => $roleData['name'],
-                    'guard_name' => $roleData['guard_name'],
+                    'name' => $rolePlusPermission['name'],
+                    'guard_name' => $rolePlusPermission['guard_name'],
                 ]);
 
-                if (!empty($roleData['permissions'])) {
-                    $permissionModels = collect($roleData['permissions'])
+                if (! blank($rolePlusPermission['permissions'])) {
+                    $permissionModels = collect($rolePlusPermission['permissions'])
                         ->map(fn ($permission) => $permissionModel::firstOrCreate([
                             'name' => $permission,
-                            'guard_name' => $roleData['guard_name'],
+                            'guard_name' => $rolePlusPermission['guard_name'],
                         ]))
                         ->all();
 
@@ -62,21 +49,14 @@ class ShieldSeeder extends Seeder
         }
     }
 
-    /**
-     * Directe permissies aanmaken.
-     *
-     * @param string $directPermissions JSON string van directe permissies
-     */
     public static function makeDirectPermissions(string $directPermissions): void
     {
-        $permissions = json_decode($directPermissions, true);
-
-        if (!empty($permissions)) {
-            /** @var class-string<Permission> $permissionModel */
+        if (! blank($permissions = json_decode($directPermissions, true))) {
+            /** @var Model $permissionModel */
             $permissionModel = Utils::getPermissionModel();
 
             foreach ($permissions as $permission) {
-                if ($permissionModel::whereName($permission['name'])->doesntExist()) {
+                if ($permissionModel::whereName($permission)->doesntExist()) {
                     $permissionModel::create([
                         'name' => $permission['name'],
                         'guard_name' => $permission['guard_name'],
