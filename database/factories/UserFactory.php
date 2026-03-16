6ab6636c8e4e23e595862d6b4e04678cdc673a32
Lucinde <lucinde@lucinde.nl>
2026-03-16 14:58:36 +0100

    require faker in live environments

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -33,13 +33,16 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        // fallback naar een handmatig aangemaakte Faker instantie als $this->faker null is
+        $faker = $this->faker ?: \Faker\Factory::create();
+
         return [
-            'name'             => $this->faker->name(),
-            'email'            => $this->faker->unique()->safeEmail(),
+            'name'             => $faker->name(),
+            'email'            => $faker->unique()->safeEmail(),
             'email_verified_at'=> now(),
             'password'         => static::$password ??= Hash::make('password'),
             'remember_token'   => Str::random(10),
-            'therapist_id'     => null, // standaard geen therapist
+            'therapist_id'     => null,
         ];
     }
 
